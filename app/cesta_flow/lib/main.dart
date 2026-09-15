1274196428271a5baca172643cbb6b4fb4a70367
NicolasFSouza <nicolasouza200@gmail.com>
2026-09-14 21:19:12 -0300

    validador cidade e estado

--- app/cesta_flow/lib/main.dart
+++ app/cesta_flow/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'features/customer/presentation/customer_registration.dart';
+
 void main() {
   runApp(const MainApp());
 }
@@ -9,8 +11,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
-    );
+    return const MaterialApp(home: CustomerRegistration());
   }
 }
