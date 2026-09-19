ddfa3ad565377839cd243b3dfc0fc56b4f3e58e0
Samuel <samuel.scardoso2004@gmail.com>
2026-09-19 20:58:47 -0300

    feat: criação do arquivo de cores

---
+++ app/cesta_flow/lib/core/constants/colors/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF1C5631);
+  static const Color secondary = Color(0xFFF97316);
+  static const Color text = Color(0xFF404942);
+  static const Color background = Color(0xFFF7FAF5);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+}
